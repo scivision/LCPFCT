fc914713aa252d604b4b2221f5dc019fb8dcdd9b
michael <scienceopen@users.noreply.github.com>
2016-05-13 19:59:25 -0400

    modules

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 import subprocess
 import setuptools #needed to enable develop
-from os.path import join
-from glob import glob
 
 try:
     subprocess.run(['conda','install','--yes','--file','requirements.txt'])
 except Exception as e:
     print('you will need to install packages in requirements.txt')
-#%%
-fortranfiles=glob('*.f')
 #%%
 with open('README.rst','r') as f:
 	long_description = f.read()
@@ -17,10 +13,17 @@ with open('README.rst','r') as f:
 from numpy.distutils.core import setup,Extension
 
 ext=[Extension(name='lcpfct',
-               sources=fortranfiles,
+               sources=['lcpfct.f','gasdyn.f'],
                f2py_options=['--quiet'],
                extra_f77_compile_args=['-Wno-unused-label']
-)]
+               ),
+Extension(name='shock',
+               sources=['shock.f','gasdyn.f','lcpfct.f'],
+               f2py_options=['--quiet'],
+               extra_f77_compile_args=['-Wno-unused-label']
+               )
+    ]
+
                #include_dirs=[root],
                #library_dirs=[root])]
 
--- test/test.py
+++ test/test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-import lcpfct
+import shock
 from numpy.testing import assert_allclose
 
 nx = 50
-darr = lcpfct.shock(nx)
+darr = shock.shock(nx)
 
 assert_allclose([darr[500,3],darr[1500,4]],[1.8167841,80.751778])
