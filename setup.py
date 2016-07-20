eff75d2b4a7e7f1a585939b654e9725d17d5739d
scienceopen <scienceopen@users.noreply.github.com>
2016-07-20 14:20:17 -0400

    template

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import subprocess
 import setuptools #needed to enable develop
 
 try:
-    subprocess.run(['conda','install','--yes','--file','requirements.txt'])
+    subprocess.call(['conda','install','--file','requirements.txt'])
 except Exception as e:
     pass
-#%%
-with open('README.rst','r') as f:
-	long_description = f.read()
 #%%
 from numpy.distutils.core import setup,Extension
 
@@ -35,9 +32,9 @@ Extension(name='fast2d',
 #%% install
 setup(name='pylcpfct',
 	 description='Python wrapper for LCPFCT model',
-	 long_description=long_description,
 	 author='Michael Hirsch',
 	 url='https://github.com/scienceopen/pylcpfct',
       ext_modules=ext,
 	  install_requires=[],
+    packages=['pylcpfct'],
       )
