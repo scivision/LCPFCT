a43531a70f29a7414ada994c53e4799caccfba1b
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-10-04 21:49:45 -0400

    auto mingw

--- setup.py
+++ setup.py
@@ -2,6 +2,15 @@
 import setuptools  # noqa: F401
 from numpy.distutils.core import setup, Extension
 from pathlib import Path
+import os
+
+
+if os.name == 'nt':
+    sfn = Path(__file__).parent / 'setup.cfg'
+    stxt = sfn.read_text()
+    if '[build_ext]' not in stxt:
+        with sfn.open('a') as f:
+            f.write("[build_ext]\ncompiler = mingw32")
 
 R = Path(__file__).parent / 'src'
 
